40377e934fdbb5ea5c2846e731450bd32ef05175
Eduardo Nascimento <eduardo.san.nascimento@gmail.com>
2025-05-26 23:14:52 -0300

    Primeiro dia estudando
    Tive pouco tempo porque comecei tarde e tenho que acordar cedo amanhã.. boa noite!! amanhã estudo mais.

---
+++ introducao/HelloWorld.java
@@ -0,0 +1,10 @@
+package introducao;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Olá, Mundo!");
+    }
+
+}
---
+++ variaveis/Variavel.java
@@ -0,0 +1,16 @@
+package variaveis;
+
+public class Variavel {
+    
+    public static void main(String[] args) {
+        
+        String nome = "Matheus";
+        Number idade = 20;
+
+        System.out.println(nome + " tem " + idade + " anos.");
+
+        Long numeroMegaGrande = 99999999999999L;
+
+        System.out.println(numeroMegaGrande);
+    }
+}
